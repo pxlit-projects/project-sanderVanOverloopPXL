4580bf176dc3d0579e362f0e2e66d044ceb19700
sanderVanOverloopPXL <sander.VanOverloop@student.pxl.be>
2024-12-20 10:40:23 +0100

    quickfix

--- frontend-web/src/environments/environment.development.ts
+++ frontend-web/src/environments/environment.development.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  apiUrl: 'api/customers',
+  postUrl: 'http://localhost:8086/post/api/post',
+  reviewApiUrl: 'http://localhost:8086/review/api/review',
+  commentApiUrl: 'http://localhost:8086/comment/api/comment'
 };
